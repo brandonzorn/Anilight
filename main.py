b1eadd0a2f1a38a5df31d331a24c2d8ad0f8df5e
bzorn <v.klimenko.2137@gmail.com>
2021-11-28 10:54:12 +0300

    2

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ from static import *
 URL = 'https://shikimori.one'
 URL_API = 'https://shikimori.one/api'
 URL_TOKEN = 'https://shikimori.one/oauth/token'
-HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/90.0.4430.93 Safari/537.36',
-           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
-                     'application/signed-exchange;v=b3;q=0.9'}
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'}
 
 
 class App:
